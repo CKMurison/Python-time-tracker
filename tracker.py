f9e64877c146f06ec68323e36495d6480d1cf46c
Cameron Murison <cmuris4@gmail.com>
2024-02-29 11:27:22 +0000

    Changed everything pretty much. Still similar logic to what I thought before but old library did not work on Mac. Have found youtube video of a guy using this library and mostly understand the code, still a bit unsure about the 2nd if statement but I think that is a NSWorkspace one that is for getting exact names of tabs rather than just saying the browser name

--- tracker.py
+++ tracker.py
@@ -1,24 +1,21 @@
-from stopwatch import Stopwatch
-import pygetwindow as getWindow
+from AppKit import NSWorkspace
+import time
 
+active_window_name = ""
 
-def track_time_and_window():
+while True:
+    new_window_name = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
+    
+    if active_window_name != new_window_name:
+        active_window_name = new_window_name
+        print(active_window_name)
+    
+    if active_window_name == 'Google Chrome':
+        text_of_MyScript = 'tell app "Google Chrome" to get the URL of the active tab of window 1'
+        s = NSAppleScript.initWithSource_(text_of_MyScript)
+        results, err = s.executeAndReturnError_(None)
+        print(results.stringValue())
+    
+    time.sleep(1)
 
-  activeWindow = getWindow
-
-  timer = Stopwatch()
-
-  time_tracker = {}
-
-  while activeWindow.isMaximised():
-    timer
-    time_tracker[activeWindow.getActiveWindow]
-
-  if activeWindow.isMinimized():
-    timer.stop()
-    duration = timer.duration
-    time_tracker[activeWindow.getActiveWindow] = duration
-  
-  print(time_tracker)
-
-track_time_and_window()
+      
